5e0fe48b93878b48a57a2a23d742068746b07490
Charles Kamiri <charleskamiri6@gmail.com>
2023-08-17 12:05:23 +0300

    Designed the sign-in page

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -9,27 +9,33 @@ const SignIn: React.FC = () => {
   return (
     <div className='sign-in'>
         <form>
-            <div className="input-box">
-                <input
-                    type="text"
-                    value={data.username} 
-                    onChange={e => setData({...data, username: e.target.value})} />
-                <label>Username</label>
+
+            <div className="main">
+                <div className="title">Sign in to your account</div>
+                <div className="input-box">
+                    <input
+                        type="text"
+                        value={data.username} 
+                        onChange={e => setData({...data, username: e.target.value})} />
+                    <label className={data.username.length !== 0 ? 'active' : ''}>Username</label>
+                </div>
+
+                <div className="input-box">
+                    <input
+                        type="password"
+                        value={data.password}
+                        onChange={e => setData({...data, password: e.target.value})} />
+                    <label className={data.password.length !== 0 ? 'active' : ''}>Password</label>
+                </div>
+
+                <button
+                    type='submit'
+                    onClick={() => sign_in(data)}>
+                        Log in
+                </button>
             </div>
 
-            <div className="input-box">
-                <input
-                    type="password"
-                    value={data.password}
-                    onChange={e => setData({...data, password: e.target.value})} />
-                <label>Password</label>
-            </div>
-
-            <button
-                type='submit'
-                onClick={() => sign_in(data)}>
-                    Log in
-            </button>
+            <div className="info">Note</div>
         </form>
     </div>
   )
